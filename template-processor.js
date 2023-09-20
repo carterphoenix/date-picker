c05039e8329a78596ef69fc840a7efc84f3a96b9
tubbywalnut <102625132+tubbywalnut@users.noreply.github.com>
2023-09-20 13:00:19 -0400

    Update template-processor.js

---
+++ template-processor.js
@@ -0,0 +1,11 @@
+function TemplateProcessor(template) {
+    this.template = template;
+  }
+  
+  TemplateProcessor.prototype.fillIn = function (dictionary) {
+    // Use a regular expression to find all instances of {{property}}
+    return this.template.replace(/{{(.*?)}}/g, (match, property) => {
+      // Check if the property exists in the dictionary
+      return dictionary[property] || '';
+    });
+  };
